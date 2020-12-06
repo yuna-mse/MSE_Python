1173be67c1185afc3a4890595abfdaa57a55f8c2
yuna cho <dbskdbsfmlzz@gmail.com>
2020-12-07 08:39:51 +0900

    added file

---
+++ ex070.py
@@ -0,0 +1,13 @@
+# >>> 방법1.
+data = [2, 4, 3, 1, 5, 10, 9]
+data.sort()                          
+#sort() 메서드를 사용하여 data를 오름차순 정렬한다.
+print(data)
+#data를 출력한다.
+
+# >>> 방법2.
+data = [2, 4, 3, 1, 5, 10, 9]
+data2 = sorted(data)              
+#data 에 대입된 리스트를 sorted 함수를 사용해 오름차순으로 정렬하고 그 결과를 새로운 변수 data2에 대입한다.
+print(data2)
+#data2를 출력한다.                    
