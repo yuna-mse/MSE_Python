0b2782716cbad0913710f671acc1b4cadd2094b9
yuna cho <dbskdbsfmlzz@gmail.com>
2020-12-07 08:46:49 +0900

    added file

---
+++ ex260.py
@@ -0,0 +1,11 @@
+# 는 코드, # >>>는 코드 설명입니다.
+
+#class OMG :
+#    def print():                        
+    # >>>프린트 함수에는 인자가 없다.
+#        print("oh my god")
+        
+#mystock = OMG()
+# >>>OMG라는 클래스로부터 객체를 생성하고 mystock에 대입한다.
+#mystock.print()   # >>>이 코드를 파이썬 인터프리터가 OMG.print(mystock)로 바꿔 인식한다.                   
+# >>>그런데 print 함수에 인자가 없기때문에 에러가 발생한다.
